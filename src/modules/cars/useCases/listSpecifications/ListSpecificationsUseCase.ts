44580e0c8bc53e610cb93e751dff63a398f70965
José Azevedo <joseazevedo47@hotmail.com>
2021-05-20 17:11:01 -0300

    Specification listing refactoring

--- src/modules/cars/useCases/listSpecifications/ListSpecificationsUseCase.ts
+++ src/modules/cars/useCases/listSpecifications/ListSpecificationsUseCase.ts
@@ -1,11 +1,17 @@
+import { inject, injectable } from 'tsyringe';
+
 import { Specification } from '../../entities/Specification';
 import { ISpecificationsRepository } from '../../repositories/ISpecificationsRepository';
 
+@injectable()
 class ListSpecificationsUseCase {
-    constructor(private specificationsRepository: ISpecificationsRepository) {}
+    constructor(
+        @inject('SpecificationsRepository')
+        private specificationsRepository: ISpecificationsRepository
+    ) {}
 
-    execute(): Specification[] {
-        const allSpecifications = this.specificationsRepository.list();
+    async execute(): Promise<Specification[]> {
+        const allSpecifications = await this.specificationsRepository.list();
         return allSpecifications;
     }
 }
--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -2,17 +2,17 @@ import { Router } from 'express';
 
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import { CreateSpecificationController } from '../modules/cars/useCases/createSpecification/CreateSpecificationController';
-import { listSpecificationsController } from '../modules/cars/useCases/listSpecifications';
+import { ListSpecificationsController } from '../modules/cars/useCases/listSpecifications/ListSpecificationsController';
 
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
+const listSpecificationsController = new ListSpecificationsController();
+
 specificationsRoutes.use(ensureAuthenticated);
 specificationsRoutes.post('/', createSpecificationController.handle);
 
-specificationsRoutes.get('/', (request, response) => {
-    listSpecificationsController.handle(request, response);
-});
+specificationsRoutes.get('/', listSpecificationsController.handle);
 
 export { specificationsRoutes };
